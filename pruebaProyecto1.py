162552cac3ae1ab3999f153ea6d8a9363bc34af0
ReyRod15 <rod.reynolds1505@gmail.com>
2025-05-25 20:08:19 -0600

    Implementacion de funcion para actualizar precios y validador de precios

--- pruebaProyecto1.py
+++ pruebaProyecto1.py
@@ -81,8 +81,8 @@ def main():
                 id_,
                 datos["nombre"],
                 datos["cantidad"],
-                f"{datos['precio_unit']:.2f}",
-                f"{datos['precio_total']:.2f}"
+                f"{datos['precio_unit']:.2f}$",
+                f"{datos['precio_total']:.2f}$"
             ]
             precios_totales += datos['precio_total']   #Acumulador de los totales de cada producto
             tabla_productos.append(fila_id)
@@ -93,23 +93,72 @@ def main():
         print(tabulate(tabla_productos, headers=['ID', 'Producto', 'Cantidad', 'Precio Unitario', 'Precio Total'], tablefmt='grid'))
 
     def borrar_producto(inventario):
-        print("\nEscriba el id del producto que desea borrar, las opciones son las siguientes: ")
+        print("\nEscriba el ID del producto que desea borrar, las opciones son las siguientes: ")
         mostrar_productos(inventario)
-        producto_borrar = str(input("--->"))
+        producto_borrar = str(input("Escriba el ID: "))
         for i in inventario.keys():
-            print(i)
             if producto_borrar == i:
                 del inventario[producto_borrar]
                 print("Producto borrado con exito")
                 break
         guardar_inventario(inventario)
 
+    def pedir_precio_valido(mensaje="Nuevo precio: "):
+        while True:
+            precio_nuevo = input(mensaje).strip()
+            if precio_nuevo == "":
+                print("No puedes dejarlo vacío.")
+                continue
+            if precio_nuevo.isdigit():
+                precio = int(precio_nuevo)
+                if precio > 0:
+                    return precio
+                else:
+                    print("El precio debe ser mayor que cero.")
+            else:
+                print("Ingresa solo números enteros positivos.")
+
     def actu_precios(inventario):
-        print("Desea:")
-        time.sleep(0.3)
-        print("1. Actualizar todos precios")
-        print("2. Cambiar el precio de un producto especifico")
-        opcion = int(input("Escriba el numero de la opcion que desea ejecutar: "))
+        while True:
+            print("Desea:")
+            time.sleep(0.3)
+            print("1. Actualizar todos precios")
+            print("2. Cambiar el precio de un producto especifico")
+            opcion = input("Escriba el numero de la opcion que desea ejecutar: ")
+                        
+            if opcion == "1":
+                for id_, datos in inventario.items():
+                    print(f"A que precio desea cambiar el siguiente producto '{datos["nombre"]}'")
+                    print(f"Precio actual >>> {datos["precio_unit"]}")
+                    try:
+                        datos["precio_unit"] = pedir_precio_valido("Nuevo precio: ")
+                        print(f"Precio de '{datos["nombre"]}', cambiado con exito a {datos["precio_unit"]}")
+                        print()
+                    except ValueError:
+                        print("Escribe un caracter valido")
+                guardar_inventario(inventario)
+                break
+            elif opcion == "2":
+                print("\nProductos disponibles")
+                time.sleep(0.5)
+                mostrar_productos(inventario)
+                while True:
+                    eleccion = str(input("Escriba el ID: ")).capitalize()
+                    if eleccion in inventario:
+                        datos = inventario[eleccion]
+                        precio_anterior = datos["precio_unit"]
+                        nuevo_precio = pedir_precio_valido(f"Nuevo precio para {datos['nombre']}: ")
+                        datos["precio_unit"] = int(nuevo_precio)
+                        print(f"Precio de '{datos["nombre"]}' cambiado con exito de {precio_anterior}$ a {datos["precio_unit"]}$")
+                        guardar_inventario(inventario)
+                        break
+                    else:
+                        print("Id no encontrado, intentelo de nuevo")
+                break
+            else:
+                print()
+                print('Opcion invalida, escoga 1 o 2')
+
 
     
     inventario = cargar_inventario()
@@ -124,45 +173,45 @@ def main():
             "5. Mandar tabla en archivo Excel para imprimir",
             "6. Salir"
         ]
-
         opcion = 0
-        while opcion not in [6]:
+        while opcion != 6:
             print()
             for linea in presentacion:
                 for letra in linea:
                     print(letra, end='', flush=True)
                     time.sleep(0.02)
                 print()
-            opcion = int(input("\nEscoge el numero de las opciones de lo que deseas hacer: "))
-            if opcion == 1:
-                mostrar_productos(inventario)
-                print("Presiona cualquier tecla para continuar...")
-                msvcrt.getch()
-            elif opcion == 2:
-                agregar_producto(inventario)
-                print("Presiona cualquier tecla para continuar...")
-                msvcrt.getch()
-            elif opcion == 3:
-                borrar_producto(inventario)
-                print("Presiona cualquier tecla para continuar...")
-                msvcrt.getch()
-            elif opcion == 4:
-                #Funcion por crearse
-                print("Presiona cualquier tecla para continuar...")
-                msvcrt.getch()
-            elif opcion == 5:
-                #Funcion por crearse
-                print("Presiona cualquier tecla para continuar...")
-                msvcrt.getch()
-            elif opcion == 6:
-                [print(c, end='', flush=True) or time.sleep(0.1) for c in "Saliendo del Programa, Gracias por todo :)"]
-            else:
-                print("Escoge una opcion valida")
-                time.sleep(1)
-                print("")
-
-
-            
+            try:
+                opcion = int(input("\nEscoge el numero de las opciones de lo que deseas hacer: "))
+                if opcion == 1:
+                    mostrar_productos(inventario)
+                    print("Presiona cualquier tecla para continuar...")
+                    msvcrt.getch()
+                elif opcion == 2:
+                    agregar_producto(inventario)
+                    print("Presiona cualquier tecla para continuar...")
+                    msvcrt.getch()
+                elif opcion == 3:
+                    borrar_producto(inventario)
+                    print("Presiona cualquier tecla para continuar...")
+                    msvcrt.getch()
+                elif opcion == 4:
+                    actu_precios(inventario)
+                    print("Presiona cualquier tecla para continuar...")
+                    msvcrt.getch()
+                elif opcion == 5:
+                    #Funcion por crearse
+                    print("Presiona cualquier tecla para continuar...")
+                    msvcrt.getch()
+                elif opcion == 6:
+                    [print(c, end='', flush=True) or time.sleep(0.1) for c in "Saliendo del Programa, Gracias por todo :)"]
+                else:
+                    print("Escoge una opcion valida")
+                    time.sleep(1)
+                    print("")
+            except ValueError:
+                print()
+                print("Selecciona una opcion valida")
     codeRunner()
 
 if __name__ == "__main__":
