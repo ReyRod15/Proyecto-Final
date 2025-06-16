08d94e0524915b3a92800b2bc1a7948155918f91
ReyRod15 <rod.reynolds1505@gmail.com>
2025-06-16 15:38:12 -0600

    Modulo archivo, funciones de cargar y guardar

---
+++ archivo.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+ARCHIVO = os.path.join(os.path.dirname(os.path.abspath(__file__)), "inventario.json")
+def cargar_inventario():
+    if os.path.exists(ARCHIVO):
+        with open(ARCHIVO, "r") as f:
+            return json.load(f)
+    return {}
+
+def guardar_inventario(inventario):
+    with open(ARCHIVO, "w") as f:
+        json.dump(inventario, f, indent=4)
